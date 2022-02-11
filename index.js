96fbdb6db46d60389714b60c305d0831803111e3
Gak Tau Bang <74234273+WilliamXixi@users.noreply.github.com>
2022-02-11 09:43:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ owner = '994407170763'
 fake = 'Ocaa && Will'
 numbernye = '0'
 waktu = '-'
-WillTest= 'One In the mountains there is only one male tiger as well as a relationship, in relationships there is only 1 male if he has more, some will leave and some will settle down'
 autovn = true
 alasan = '-'
 randomUser = {}
@@ -111,15 +110,7 @@ const checkSCommand = (id) => {
 	Object.keys(_scommand).forEach((i) => {
 		if (_scommand[i].id === id) {
 			status = true
-		}
-	})
-	return status
-}
-module.exports = hexa = async (hexa, mek) => {
-try {
-        if (!mek.hasNewMessage) return hexa.setStatus(`Runtime ${kyun(process.uptime())} | Mode: ${banChats ? 'SELF-MODE' : 'PUBLIC-MODE'} | ${WillTest}`)
-
-        mek = mek.messages.all()[0]
+		}) 
 		if (!mek.message) return
 		if (mek.key && mek.key.remoteJid == 'status@broadcast') return
 if (mek.key.id.startsWith('3EB0') && mek.key.id.length === 12) return
